a72f0df55465a59f6735c17ba52dcc88ab9a8ccc
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-04-16 23:03:33 -0700

    Restored the program.cs file to its preset project code + added the CORS policy to middlewear, still doesn't activates CORS

--- libre-pansador-api/libre-pansador-api/Program.cs
+++ libre-pansador-api/libre-pansador-api/Program.cs
@@ -1,36 +1,42 @@
-using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+
+const string AllowSpecificOrigins = "AllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy(name: "AllowSpecificOrigins",
-                      builder =>
+    options.AddPolicy(name: AllowSpecificOrigins,
+                      policy =>
                       {
-                          builder.WithOrigins("http://localhost:8080")
-                                 .AllowAnyHeader()
-                                 .AllowAnyMethod()
-                                 .AllowCredentials()
-                                 .SetIsOriginAllowedToAllowWildcardSubdomains()
-                                 .SetIsOriginAllowed(origin => true);
+                          policy.WithOrigins("https://localhost:80",
+                                             "http://localhost:80")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
                       });
 });
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowSpecificOrigins");
-
 app.UseHttpsRedirection();
+
+app.UseCors(AllowSpecificOrigins);
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
