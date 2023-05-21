bccafdf8c722bef82f710a2de7e0bd63397a5198
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-05-20 22:20:09 -0700

    + Employees CRUD new Create and Delete methods
    DataBase:
    +CREATE User script for the client api user

---
+++ DB/client_api DDL script.sql
@@ -0,0 +1,5 @@
+CREATE USER client_api WITH PASSWORD 'password';
+
+GRANT SELECT, UPDATE ON public."Cards" TO client_api;
+GRANT SELECT, DELETE, INSERT ON public."customers" TO client_api;
+GRANT SELECT, DELETE, INSERT ON public."employees" TO client_api;
--- libre-pansador-api/libre-pansador-api/CRUD/Employees.cs
+++ libre-pansador-api/libre-pansador-api/CRUD/Employees.cs
@@ -1,5 +1,6 @@
 ﻿using libre_pansador_api.Interfaces;
 using libre_pansador_api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace libre_pansador_api.CRUD
 {
@@ -12,9 +13,33 @@ namespace libre_pansador_api.CRUD
             this._dbContext = dbContext;
         }
 
-        public Models.Employee? Read(string userName) 
+        public Models.Employee? Read(string userName)
         {
-            return this._dbContext.Employees.Find(userName);
+            var employee = this._dbContext.Employees
+                .AsEnumerable()
+                .FirstOrDefault(e => e.UserName == userName);
+            return employee;
+        }
+
+        public Models.Employee? Create(Models.Employee newEmployee)
+        {
+            //newEmployee.IsAdmin = false;
+            this._dbContext.Employees.Add(newEmployee);
+            this._dbContext.SaveChanges();
+            return Read(newEmployee.UserName);
+        }
+
+        public Models.Employee? Delete(string userName)
+        {
+            Models.Employee? employeeToDelete = this._dbContext.Employees
+                .AsEnumerable()
+                .FirstOrDefault(e => e.UserName == userName);
+            if (employeeToDelete == null)
+                return null;
+            string sql = "DELETE FROM employees WHERE user_name = @p0";
+            int rowsAffected = _dbContext.Database.ExecuteSqlRaw(sql, employeeToDelete.UserName);
+
+            return rowsAffected > 0 ? employeeToDelete : null;
         }
     }
 }
