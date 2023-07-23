f228ca5f2340f5f9da0039372253a13f52c7cdd7
Andres Antonio Castro Beltran <andres435b@gmail.com>
2023-07-22 22:16:00 -0700

    Add IClientAppsServices Add ClientAppsService with  + Read  + Create  + Delete The body functions are still empty

---
+++ libre-pensador-api/libre-pensador-api/CRUD/ClientAppsService.cs
@@ -0,0 +1,30 @@
+﻿using libre_pensador_api.Interfaces;
+using libre_pensador_api.Models;
+
+namespace libre_pensador_api.CRUD
+{
+    public class ClientAppsService : IClientAppsService
+    {
+        private readonly CafeLibrePensadorDbContext _dbContext;
+
+        public ClientAppsService(CafeLibrePensadorDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public Task CreateAsync(string newAppName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int appId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClientApp> ReadAppAsync(int appId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ libre-pensador-api/libre-pensador-api/Interfaces/IClientAppsService.cs
@@ -0,0 +1,11 @@
+﻿using libre_pensador_api.Models;
+
+namespace libre_pensador_api.Interfaces
+{
+    public interface IClientAppsService
+    {
+        Task<ClientApp> ReadAppAsync(int appId);
+        Task CreateAsync(string newAppName);
+        Task DeleteAsync(int appId);
+    }
+}
